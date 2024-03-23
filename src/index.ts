cfcbae1c6f57130e6f3189c6872d52f2c4a06598
shoaib665583 <shoaib@cablesmith.in>
2024-03-24 00:30:28 +0530

    add logger functions

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export class log {
+    static success (msg: string) {
+        console.log(`%c ${msg} , 'color: green'`);
+        
+    }
+
+    static danger (msg: string) {
+        console.log(`%c ${msg} , 'color: red'`);
+    }
+
+    static info(msg: string) {
+        console.log(`%c ${msg} , 'color: black; background: yellow'`);
+    }
+}
